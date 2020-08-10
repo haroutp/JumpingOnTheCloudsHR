aabc300013293a994fa82d40407bc821152ee2fe
Harout Parseghian <haroutparseghain@gmail.com>
2020-08-11 02:59:37 +0400

    add solution to problem

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JumpingOnTheClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(jumpingOnClouds(new int[7] {0, 0, 1, 0, 0, 1, 0}));
+        }
+
+        static int jumpingOnClouds(int[] c) 
+        {
+            int jumpNum = 0;
+            int i = 0;
+            
+            while (i < c.Length - 1)
+            {
+                if(i + 2 == c.Length || c[i + 2] == 1)
+                {
+                    i++;
+                    jumpNum++;
+                }
+                else
+                {
+                    i += 2;
+                    jumpNum++;
+                }
+            }
+            return jumpNum;
+        }
+    }
+}
